21c15855e9bbc44aec2096b2ef9abb12da6080f6
jasonxuwang <jasonxuwang@qq.com>
2020-11-06 20:00:04 +0800

    restructure

--- TCPSocket.h
+++ TCPSocket.h
@@ -10,7 +10,7 @@ class TCPSocket{
         int32_t as_server(int32_t port);
         int32_t as_client(char* ipstr,int32_t port);
         int32_t get_socket_fd();
-        void set_socket_fd(int32_t i_socket_fd){
+        void set_socket_fd(int32_t i_socket_fd)
         // wrapper for socket functions
 
         int32_t read(char* start);
@@ -28,8 +28,4 @@ class TCPSocket{
         char recvbuf[BUFFSIZE], sendbuf[BUFFSIZE];
         struct sockaddr_in m_socket_addr;
 	    struct sockaddr_in m_client_addr;
-
-
-    
-
 };
